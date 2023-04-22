36f9bf46689bab876bb4f046299cf4323424ed7c
fatston <cliftonkor@gmail.com>
2023-04-23 00:18:28 +0800

    zaxc

---
+++ Task1B/k8s/task1b_setup.sh
@@ -0,0 +1,10 @@
+# Place your commands here
+kind create cluster --name kind-1 --config kind/cluster-config.yaml
+
+kubectl apply -f manifests/k8s/backend-deployment.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+kubectl apply -f manifests/k8s/backend-service.yaml
+
+kubectl apply -f manifests/k8s/backend-ingress.yaml
